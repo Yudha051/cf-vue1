9b729c1b01d4aae6a5d6a5136d317a6ace0cbf91
Muhammad Yudha Prianto <muhammadyudhaprianto52@gmail,com>
2026-07-05 06:41:49 +0700

    init

--- migrations/0002_create_users.sql
+++ migrations/0002_create_users.sql
@@ -1,4 +1,4 @@
--- Migration created for D1
+-- Migration number: 0002 	 2026-07-04T23:37:33.999Z
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
